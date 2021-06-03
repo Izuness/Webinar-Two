8845033c79a429644083b1ee3d75445932ea248b
Izune <Izune@DESKTOP-4CTQIAJ>
2021-06-03 14:59:25 +0300

    Добавьте файлы проекта.

---
+++ Webinar Two/Webinar Two.cpp
@@ -0,0 +1,58 @@
+﻿
+/* Задание 1
+
+short int a = 32767
+int b = 3
+long long = 4'000'000'000'000
+char name = 255
+bool = false
+float = 3.2E+38
+double = 15.5432
+
+*/
+
+#include <iostream>
+#include <array>
+#include "Webinar Two.h"
+#include <stdio.h>
+
+//Dz Two
+
+enum symbols { cross, zero, empty };
+
+// Dz Three
+
+int main()
+{
+	
+	
+	
+	int a[3][3] = { {_, X, 0}
+					{0, X, _}
+					{0, _, X}
+	};
+
+int n = 3;
+int m = 3;
+	
+	
+	
+// Тут мои предыдущие попытки
+
+int turn;
+int player, winner, looser;
+
+/*char board[3][3] = {{"1", "2", "3"},
+                      {"4", "5", "6"},
+                      {"7", "8", "9"}
+                   };
+
+const int size = 3;
+char Array3[size][size];
+
+std::cout << Array[3][3] << " " << Array[2][1];
+
+
+return 0;
+}
+*/
